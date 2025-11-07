90d70855bfc6418e9eff0e13184116fcd79653b1
razi0906 <146936554+razi0906@users.noreply.github.com>
2025-11-08 00:28:31 +0530

    Delete aws_notes_api directory

--- aws_notes_api/terraform/environment.tfvars
+++
@@ -1,6 +0,0 @@
-environment    = "development" # to be used for tags
-
-default_tags = {
-  owner       = "razi.ahmed"
-  description = "Deployed by Terraform - DO NOT MODIFY"
-}
--- aws_notes_api/terraform/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  # merging additional tags to default_tags from tfvars
-  default_tags = merge(
-    var.default_tags, { environment = var.environment, product = var.product }
-  )
-}
